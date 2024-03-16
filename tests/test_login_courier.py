fd185f31098ff2d2922ea8879b0c0a48d96d64db
zar <zarinasadykova.m@gmail.com>
2024-03-16 15:49:47 +0500

    исправила

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -1,55 +1,37 @@
 import allure
 import requests
+from Sprint_7_api.project_url import Urls, Endpoints
+from Sprint_7_api.data_test import TestUsers
 
 
 class TestLoginCourier:
 
-    @staticmethod
-    def login_courier(payload):
-        return requests.post('http://qa-scooter.praktikum-services.ru/api/v1/courier/login', data=payload)
-
     @allure.title('Успешная авторизация возвращает id')
     def test_courier_login_return_id(self):
-        payload = {
-            "login": "yeymmcbbkx",
-            "password": "ojugthnwnm"
-        }
-        response = self.login_courier(payload)
-
+        payload = TestUsers.positive_login
+        response = requests.post(f'{Urls.URL}{Endpoints.LOGIN_COURIER}', json=payload)
         assert response.status_code == 200 and 'id' in response.json()
 
     @allure.title('Авторизация без логина - "Недостаточно данных для входа"')
     def test_courier_login_without_login_negative_result(self):
-        payload = {
-            "login": "",
-            "password": "ojugthnwnm"
-        }
-        response = self.login_courier(payload)
+        payload = TestUsers.user_without_login
+        response = requests.post(f'{Urls.URL}{Endpoints.LOGIN_COURIER}', json=payload)
         assert response.json() == {'code': 400, 'message': 'Недостаточно данных для входа'}
 
     @allure.title('Авторизация без пароля - "Недостаточно данных для входа"')
     def test_courier_login_without_password_negative_result(self):
-        payload = {
-            "login": "yeymmcbbkx",
-            "password": ""
-        }
-        response = self.login_courier(payload)
+        payload = TestUsers.user_without_password
+        response = requests.post(f'{Urls.URL}{Endpoints.LOGIN_COURIER}', json=payload)
         assert response.json() == {'code': 400, 'message': 'Недостаточно данных для входа'}
 
     @allure.title('Авторизация неверным логином - "Учетная запись не найдена"')
     def test_courier_login_wrong_login_negative_result(self):
-        payload = {
-            "login": "00000000",
-            "password": "ojugthnwnm"
-        }
-        response = self.login_courier(payload)
+        payload = TestUsers.wrong_login
+        response = requests.post(f'{Urls.URL}{Endpoints.LOGIN_COURIER}', json=payload)
         assert response.json() == {'code': 404, 'message': 'Учетная запись не найдена'}
 
     @allure.title('Авторизация неверным паролем - "Учетная запись не найдена"')
     def test_courier_login_wrong_password_negative_result(self):
-        payload = {
-            "login": "yeymmcbbkx",
-            "password": "000000000"
-        }
-        response = self.login_courier(payload)
+        payload = TestUsers.wrong_password
+        response = requests.post(f'{Urls.URL}{Endpoints.LOGIN_COURIER}', json=payload)
         assert response.json() == {'code': 404, 'message': 'Учетная запись не найдена'}
