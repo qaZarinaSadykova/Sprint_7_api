eebd6bb36613a91092363ac4cdd3483eeb24a34c
zar <zarinasadykova.m@gmail.com>
2024-03-16 15:45:19 +0500

    перенесла урлы в отдельный файл

---
+++ project_url.py
@@ -0,0 +1,10 @@
+class Urls:
+    URL = 'http://qa-scooter.praktikum-services.ru'
+
+
+class Endpoints:
+    CREATE_COURIER = '/api/v1/courier'
+
+    LOGIN_COURIER = '/api/v1/courier/login'
+
+    CREATE_ORDER = '/api/v1/orders'
